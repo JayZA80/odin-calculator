603e66876bc04bfe04afd49509dd93203677a22e
Jay <jay.pena1224@proton.me>
2023-02-03 15:25:08 -0500

    Add event listeners to number pad
    When a user clicks a button on the number pad, The number entered will
    be stored in an object to be used for calculations.

--- script.js
+++ script.js
@@ -43,3 +43,44 @@ const multiply = (x, y) => {
 const divide = (x, y) => {
     return x / y;
 }
+
+const operate = (x, y, operator) => {
+    let firstNum = parseInt(x);
+    let secondNum = parseInt(y);
+    switch (operator) {
+        case (operator === '+'):
+            add(firstNum, secondNum);
+            break;
+        case (operator === '-'):
+            subtract(firstNum, secondNum);
+            break;
+        case (operator === '*'):
+            multiply(firstNum, secondNum);
+            break;
+        case (operator === '/'):
+            divide(firstNum, secondNum);
+            break;
+        default:
+            console.log('Something went wrong with calculating...');
+    }
+}
+
+let numPad = Array.from(document.querySelectorAll('.number'));
+
+numPad.map(button => {
+    button.addEventListener('click', e => {
+        if (num2.active === true) {
+            if (num2.value === undefined) {
+                num2.setValue(e.target.innerText);
+            } else {
+                num2.value += e.target.innerText;
+            }
+        } else {
+            if (num1.value === undefined) {
+                num1.setValue(e.target.innerText);
+            } else {
+                num1.value += e.target.innerText;
+            }
+        }
+    });
+});
